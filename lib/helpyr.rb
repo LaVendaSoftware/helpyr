351fb0dd3ae53317e9478b25e3525c84b924d0d6
Walmir <owalmirneto@gmail.com>
2026-02-07 07:17:13 -0300

    chore: add locales

--- lib/helpyr.rb
+++ lib/helpyr.rb
@@ -1,6 +1,7 @@
 require_relative "helpyr/version"
 
 module Helpyr
+  I18n.load_path += Dir[File.join(__dir__, "helpyr/locales/*.yml")]
+
   class Error < StandardError; end
-  # Your code goes here...
 end
